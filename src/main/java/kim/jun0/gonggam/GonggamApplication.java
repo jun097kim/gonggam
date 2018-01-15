d3b11d6a0df80fef6fbec35bcd6263fe77cedd2e
김준영 <jun097kim@gmail.com>
2018-01-15 10:46:59 +0900

    Initial Commit

---
+++ src/main/java/kim/jun0/gonggam/GonggamApplication.java
@@ -0,0 +1,12 @@
+package kim.jun0.gonggam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonggamApplication.class, args);
+	}
+}
---
+++ src/main/java/kim/jun0/gonggam/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kim.jun0.gonggam;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GonggamApplication.class);
+	}
+
+}
