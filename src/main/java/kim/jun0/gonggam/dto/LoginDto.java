0ffb06adaddfd89f93c30025fec8975a40ee73b5
김준영 <jun097kim@gmail.com>
2018-02-01 19:36:08 +0900

    누락된 UserVo, LoginDto 추가

---
+++ src/main/java/kim/jun0/gonggam/dto/LoginDto.java
@@ -0,0 +1,15 @@
+package kim.jun0.gonggam.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class LoginDto {
+
+	private String userId;
+	private String userPw;
+	
+}
