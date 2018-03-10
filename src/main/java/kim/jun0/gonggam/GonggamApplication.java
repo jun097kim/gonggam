5e1ea8943f67b941c97974af9614f37c597dd471
jun097kim <jun097kim@gmail.com>
2018-03-10 14:03:56 +0900

    AWS 배포

--- src/main/java/kim/jun0/gonggam/GonggamApplication.java
+++ src/main/java/kim/jun0/gonggam/GonggamApplication.java
@@ -2,10 +2,17 @@ package kim.jun0.gonggam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GonggamApplication {
-
+public class GonggamApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GonggamApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GonggamApplication.class, args);
 	}
